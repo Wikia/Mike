fffeb0f279dfafbcd54918b3530ab758944c9f9d
macbre <maciej.brencz@gmail.com>
2018-11-26 11:12:24 +0100

    Add setup.py, Makefile and .travis.yml

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Mike
 Mycroft Holmes (High-Optional, Logical, Multi-Evaluating Supervisor) aka Mike
+
+## Install
+
+```
+virtualenv -ppython3 env
+. env/bin/activate
+pip install -e .
+```
+
+
+To install development dependencies run `make` inside virtualenv.
+
+## Running
+
+> TODO
+
+## Config file
+
+> TODO
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+from mycroft_holmes import VERSION
+
+# @see https://github.com/pypa/sampleproject/blob/master/setup.py
+setup(
+    name='mycroft_holmes',
+    version=VERSION,
+    author='Maciej Brencz',
+    author_email='maciej.brencz@gmail.com',
+    license='MIT',
+    description='Mycroft Holmes (High-Optional, Logical, Multi-Evaluating Supervisor) aka Mike',
+    keywords='',
+    url='https://github.com/Wikia/Mike',
+    packages=find_packages(),
+    extras_require={
+        'dev': [
+            'coverage==4.5.2',
+            'pylint==2.2.0',
+            'pytest==4.0.1',
+        ]
+    },
+    install_requires=[
+    ]
+)
