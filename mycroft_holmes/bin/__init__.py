89915106302078eedaed31fb94ead3d015dc5308
macbre <maciej.brencz@gmail.com>
2018-11-29 09:26:42 +0100

    bin: support DEBUG env variable

--- mycroft_holmes/bin/__init__.py
+++ mycroft_holmes/bin/__init__.py
@@ -2,9 +2,11 @@
 Command line utilities
 """
 import logging
+from os import getenv
+
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG if getenv('DEBUG') == '1' else logging.INFO,
     format='%(asctime)s %(name)-25s %(levelname)-8s %(message)s',
     datefmt="%Y-%m-%d %H:%M:%S"
 )
