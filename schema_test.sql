422046c39838b4f7c3c90cd7497dbb9079d1024a
macbre <maciej.brencz@gmail.com>
2019-01-24 11:22:21 +0100

    schema_test.sql: only drop the table if it exists

--- schema_test.sql
+++ schema_test.sql
@@ -1,5 +1,5 @@
 -- This file contains a schema and sample rows for MysqlSource tests
-DROP TABLE `mike_test`;
+DROP TABLE IF EXISTS `mike_test`;
 
 CREATE TABLE `mike_test` (
   `user_id` int(9) NOT NULL AUTO_INCREMENT,
