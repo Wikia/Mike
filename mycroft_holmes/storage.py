2371e0569b8fe36f8e5328468b0df348576f4149
macbre <maciej.brencz@gmail.com>
2019-02-01 10:03:32 +0100

    Storage: improved DB errors logging

--- mycroft_holmes/storage.py
+++ mycroft_holmes/storage.py
@@ -3,6 +3,7 @@ Handles metrics storage
 """
 import logging
 from mysql import connector
+from mysql.connector.errors import Error as MySqlError
 
 
 class MetricsStorage:
@@ -75,17 +76,24 @@ class MetricsStorage:
         # ORDER BY timestamp desc limit 1;
         self.logger.info('Reading metric for "%s": %s', feature_id, feature_metric)
 
-        cursor = self.storage.cursor()
+        try:
+            cursor = self.storage.cursor()
 
-        cursor.execute(
-            'SELECT /* mycroft_holmes */ value FROM features_metrics '
-            'WHERE feature = %s and metric = %s '
-            'ORDER BY timestamp DESC LIMIT 1',
-            (feature_id, feature_metric)
-        )
+            cursor.execute(
+                'SELECT /* mycroft_holmes */ value FROM features_metrics '
+                'WHERE feature = %s and metric = %s '
+                'ORDER BY timestamp DESC LIMIT 1',
+                (feature_id, feature_metric)
+            )
 
-        row = cursor.fetchone()
-        return row[0] if row else None
+            # self.logger.debug('SQL: %s', cursor.statement)
+
+            row = cursor.fetchone()
+            return row[0] if row else None
+
+        except MySqlError as ex:
+            self.logger.error('Storage error occured: %s', ex)
+            raise ex
 
     def push(self, feature_id, feature_metrics):
         """
@@ -117,11 +125,13 @@ class MetricsStorage:
                         }
                     )
 
+                    # self.logger.debug('SQL: %s', cursor.statement)
+
             self.storage.commit()
 
             self.data = dict()
             self.logger.info('Data has been stored')
 
-        except connector.errors.Error as ex:
+        except MySqlError as ex:
             self.logger.error('Storage error occured: %s', ex)
             raise ex
